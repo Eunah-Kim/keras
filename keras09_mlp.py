83f031cc9691a1da75c2bf4ec310d9aec463e3ca
Eunah Kim <keunah1016@naver.com>
2020-01-23 15:31:28 +0900

    message

--- keras09_mlp.py
+++ keras09_mlp.py
@@ -1,7 +1,11 @@
 # 1. 데이터
 import numpy as np
-x = np.array(range(1, 101))
-y = np.array(range(1, 101))
+x = np.array([range(1, 11), range(11,21)])
+y = np.array([range(1, 11), range(11,21)])
+
+print(x.shape)  # (2, 10)
+print(y.shape)  # (2, 10)
+
 
 
 from sklearn.model_selection import train_test_split
@@ -10,11 +14,11 @@ x_train, x_test, y_train, y_test = train_test_split(
 x_val, x_test, y_val, y_test = train_test_split(
     x_test, y_test, test_size=0.5, shuffle=False, random_state=66)
 
-'''
+
 print(x_train)
 print(x_val)
 print(x_test)
-'''
+
 
 # 2. 모델 구성
 from keras.models import Sequential
@@ -23,10 +27,10 @@ from keras.layers import Dense
 model = Sequential()
 
 # model.add(Dense(5, input_dim=1))
-model.add(Dense(5, input_shape=(1,)))
+model.add(Dense(5, input_shape=(2,)))
 model.add(Dense(2))
 model.add(Dense(3))
-model.add(Dense(1))
+model.add(Dense(2))
 
 model.summary()
 
@@ -43,7 +47,7 @@ model.fit(x_train, y_train, epochs = 100, batch_size=1,
 # 4. 평가 예측
 loss, mae = model.evaluate(x_test, y_test, batch_size=1)
 print('mae: ', mae)
-
+'''
 x_prd = np.array([101,102,103])
 aaa = model.predict(x_prd, batch_size=1)
 print(aaa)
@@ -61,3 +65,4 @@ print("RMSE: ", RMSE(y_test, y_predict))
 from sklearn.metrics import r2_score
 r2_pred = r2_score(y_test, y_predict)
 print("r2: ", r2_pred)
+'''
