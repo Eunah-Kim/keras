f95cd6d115380f4b8c60279e9368c62fa3befdf9
EunahKim <keunah1016@naver.com>
2020-02-05 20:57:42 +0900

    a

--- keras24_m_parallel3.py
+++ keras24_m_parallel3.py
@@ -12,6 +12,7 @@ def split_sequence3(sequence, n_steps):
         y.append(seq_y)
     return array(X), array(y)
 
+
 in_seq1 = array([10,20,30,40,50,60,70,80,90,100])
 in_seq2 = array([15,25,35,45,55,65,75,85,95,105])
 out_seq = array([in_seq1[i] + in_seq2[i] for i in range(len(in_seq1))])
